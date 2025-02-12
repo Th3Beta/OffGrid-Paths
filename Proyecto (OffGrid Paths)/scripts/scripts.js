8b73d1aa605226b359e9c70b4f20f01bff8135e9
Th3Beta <pabloerrando152@gmail.com>
2025-02-12 11:09:38 +0100

    Add files via upload

---
+++ Proyecto (OffGrid Paths)/scripts/scripts.js
@@ -0,0 +1,38 @@
+document.querySelectorAll(".botonDesplegable").forEach(button => {
+    button.addEventListener("click", function() {
+        let menu = this.nextElementSibling;
+        if (menu.style.display === "none" || menu.style.display === "") {
+            menu.style.display = "block";
+        } else {
+            menu.style.display = "none";
+        }
+    });
+});
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+
